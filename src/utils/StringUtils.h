8014d2e3895e2cb9827469a1c1e7309a8d115a28
boooiil <boooiil@outlook.com>
2023-12-28 17:54:40 -0500

    Add split() function to StringUtils class

--- src/utils/StringUtils.h
+++ src/utils/StringUtils.h
@@ -3,12 +3,14 @@
 
 #include <regex>
 #include <string>
+#include <vector>
 
 class StringUtils {
  public:
   static std::string truncateString(std::string);
   static std::string replaceAll(std::string, std::string, std::string);
   static std::string replaceAll(std::string, std::regex, std::string);
+  static std::vector<std::string> split(std::string, std::string);
   static std::string toLowerCase(std::string str);
 };
 
