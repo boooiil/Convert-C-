e1895c662b8035706a5d42f6d7f9c3a9e8c4f22b
boooiil <boooiil@outlook.com>
2023-12-28 12:50:44 -0500

    add size; describe vars

--- src/media/MediaFile.h
+++ src/media/MediaFile.h
@@ -8,16 +8,22 @@ class MediaFile {
   MediaFile();
   ~MediaFile();
 
+  /* Filename after renaming without ext */
   std::string modifiedFileName;
+  /* Filename after renaming including extension */
   std::string modifiedFileNameExt;
+  /* Result of modifiedFileNameExt */
   std::string conversionName;
+  /* Path resolving to cwd/{series} Season {##}/{conversionName} */
   std::string conversionPath;
   std::string renamePath;
+  std::string episode;
   std::string series;
   std::string path;
   std::string ext;
 
   int number;
+  int size;
   int newSize;
   int validationSize;
   int quality;
