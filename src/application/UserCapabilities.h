997d22148120c732acf272cffab57ebe482e2caa
boooiil <boooiil@outlook.com>
2023-12-28 13:10:03 -0500

    user capabilities class def

---
+++ src/application/UserCapabilities.h
@@ -0,0 +1,27 @@
+#ifndef USER_CAPABILITIES
+#define USER_CAPABILITIES
+
+#include <vector>
+
+#include "../ffmpeg/Decoders.h"
+#include "../ffmpeg/Encoders.h"
+#include "Platform.h"
+
+class UserCapabilities {
+ public:
+  UserCapabilities(void);
+  ~UserCapabilities(void);
+
+  Platform::OS platform;
+  char* GPU_Provider;
+  std::vector<Encoders::Codec> supportedEncoders;
+  std::vector<Decoders::Codec> supportedDecoders;
+
+  /**
+   * @brief Finds the hardware details of the user's machine
+   * @return void
+   */
+  void findHardwareDetails(void);
+};
+
+#endif  // !USER_CAPABILITIES
