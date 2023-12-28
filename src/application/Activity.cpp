15f509cdf4e9e096823e92a7680d8b0e85ad9a43
boooiil <boooiil@outlook.com>
2023-12-28 12:57:59 -0500

    lowercase

--- src/application/Activity.cpp
+++ src/application/Activity.cpp
@@ -1,26 +1,26 @@
 #include "Activity.h"
 
 std::unordered_map<Activity::ActivityType, std::string> Activity::activityMap =
-    {{ActivityType::CONVERT, "CONVERT"},
-     {ActivityType::FAILED, "FAILED"},
-     {ActivityType::FAILED_CODEC, "FAILED_CODEC"},
-     {ActivityType::FAILED_CONTAINER, "FAILED_CONTAINER"},
-     {ActivityType::FAILED_CORRUPT, "FAILED_CORRUPT"},
-     {ActivityType::FAILED_FILE, "FAILED_FILE"},
-     {ActivityType::FAILED_FILE_MISSING, "FAILED_FILE_MISSING"},
-     {ActivityType::FAILED_FILE_PERMISSIONS, "FAILED_FILE_PERMISSIONS"},
-     {ActivityType::FAILED_FILE_NOT_RECOGNIZED, "FAILED_FILE_NOT_RECOGNIZED"},
-     {ActivityType::FAILED_HARDWARE, "FAILED_HARDWARE"},
+    {{ActivityType::CONVERT, "convert"},
+     {ActivityType::FAILED, "failed"},
+     {ActivityType::FAILED_CODEC, "failed_codec"},
+     {ActivityType::FAILED_CONTAINER, "failed_container"},
+     {ActivityType::FAILED_CORRUPT, "failed_corrupt"},
+     {ActivityType::FAILED_FILE, "failed_file"},
+     {ActivityType::FAILED_FILE_MISSING, "failed_file_missing"},
+     {ActivityType::FAILED_FILE_PERMISSIONS, "failed_file_permissions"},
+     {ActivityType::FAILED_FILE_NOT_RECOGNIZED, "failed_file_not_recognized"},
+     {ActivityType::FAILED_HARDWARE, "failed_hardware"},
      {ActivityType::FAILED_INVALID_AUDIO_STREAMS,
-      "FAILED_INVALID_AUDIO_STREAMS"},
-     {ActivityType::FAILED_SYSTEM, "FAILED_SYSTEM"},
-     {ActivityType::FINISHED, "FINISHED"},
-     {ActivityType::STATISTICS, "STATISTICS"},
-     {ActivityType::WAITING, "WAITING"},
-     {ActivityType::WAITING_CONVERT, "WAITING_CONVERT"},
-     {ActivityType::WAITING_STATISTICS, "WAITING_STATISTICS"},
-     {ActivityType::WAITING_VALIDATE, "WAITING_VALIDATE"},
-     {ActivityType::VALIDATE, "VALIDATE"}};
+      "failed_invalid_audio_streams"},
+     {ActivityType::FAILED_SYSTEM, "failed_system"},
+     {ActivityType::FINISHED, "finished"},
+     {ActivityType::STATISTICS, "statistics"},
+     {ActivityType::WAITING, "waiting"},
+     {ActivityType::WAITING_CONVERT, "waiting_convert"},
+     {ActivityType::WAITING_STATISTICS, "waiting_statistics"},
+     {ActivityType::WAITING_VALIDATE, "waiting_validate"},
+     {ActivityType::VALIDATE, "validate"}};
 
 Activity::Activity() {}
 Activity::~Activity() {}
