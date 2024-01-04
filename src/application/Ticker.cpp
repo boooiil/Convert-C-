2d981302bf62fd929d1e5b9215515680d17684c2
boooiil <boooiil@outlook.com>
2024-01-04 00:48:23 -0500

    increase ticker rate

--- src/application/Ticker.cpp
+++ src/application/Ticker.cpp
@@ -113,7 +113,7 @@ void Ticker::start() {
     else
       Ticker::display->print();
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
   }
 }
 
