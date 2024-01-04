7f35874668b93330026d4690a4c11c5f485e04a3
boooiil <boooiil@outlook.com>
2024-01-03 20:52:35 -0500

    add splitv

--- src/utils/ListUtils.cpp
+++ src/utils/ListUtils.cpp
@@ -10,3 +10,16 @@ std::string ListUtils::join(std::vector<std::string> list, std::string delim) {
   }
   return result;
 }
+
+std::vector<std::string> ListUtils::splitv(std::string str, std::string delim) {
+  std::vector<std::string> result;
+  std::string token;
+  size_t pos = 0;
+  while ((pos = str.find(delim)) != std::string::npos) {
+    token = str.substr(0, pos);
+    result.push_back(token);
+    str.erase(0, pos + delim.length());
+  }
+  result.push_back(str);
+  return result;
+}
