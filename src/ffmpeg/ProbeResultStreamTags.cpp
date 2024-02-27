cfaebf5dd0412fe1120ff6cb29f59c0dc2f7b37e
boooiil <boooiil@outlook.com>
2024-02-27 00:36:48 -0500

    guard against null values from probe

--- src/ffmpeg/ProbeResultStreamTags.cpp
+++ src/ffmpeg/ProbeResultStreamTags.cpp
@@ -12,18 +12,30 @@ ProbeResultStreamTags::ProbeResultStreamTags(nlohmann::json JSON) {
     Log::debug({"[ProbeResultStreamTags.cpp] title: ", JSON["title"]});
     ProbeResultStreamTags::title = JSON["title"];
   }
-  Log::debug({"[ProbeResultStreamTags.cpp] BPS: ", JSON["BPS"]});
-  ProbeResultStreamTags::BPS = JSON["BPS"];
-  Log::debug({"[ProbeResultStreamTags.cpp] NUMBER_OF_FRAMES: ",
-              JSON["NUMBER_OF_FRAMES"]});
-  ProbeResultStreamTags::NUMBER_OF_FRAMES = JSON["NUMBER_OF_FRAMES"];
-  Log::debug({"[ProbeResultStreamTags.cpp] NUMBER_OF_BYTES: ",
-              JSON["NUMBER_OF_BYTES"]});
-  ProbeResultStreamTags::NUMBER_OF_BYTES = JSON["NUMBER_OF_BYTES"];
-  Log::debug({"[ProbeResultStreamTags.cpp] _STATISTICS_WRITING_APP: ",
-              JSON["_STATISTICS_WRITING_APP"]});
-  ProbeResultStreamTags::_STATISTICS_WRITING_APP =
-      JSON["_STATISTICS_WRITING_APP"];
+
+  if (!JSON["BPS"].is_null()) {
+    Log::debug({"[ProbeResultStreamTags.cpp] BPS: ", JSON["BPS"]});
+    ProbeResultStreamTags::BPS = JSON["BPS"];
+  }
+
+  if (!JSON["NUMBER_OF_FRAMES"].is_null()) {
+    Log::debug({"[ProbeResultStreamTags.cpp] NUMBER_OF_FRAMES: ",
+                JSON["NUMBER_OF_FRAMES"]});
+    ProbeResultStreamTags::NUMBER_OF_FRAMES = JSON["NUMBER_OF_FRAMES"];
+  }
+
+  if (!JSON["NUMBER_OF_BYTES"].is_null()) {
+    Log::debug({"[ProbeResultStreamTags.cpp] NUMBER_OF_BYTES: ",
+                JSON["NUMBER_OF_BYTES"]});
+    ProbeResultStreamTags::NUMBER_OF_BYTES = JSON["NUMBER_OF_BYTES"];
+  }
+
+  if (!JSON["_STATISTICS_WRITING_APP"].is_null()) {
+    Log::debug({"[ProbeResultStreamTags.cpp] _STATISTICS_WRITING_APP: ",
+                JSON["_STATISTICS_WRITING_APP"]});
+    ProbeResultStreamTags::_STATISTICS_WRITING_APP =
+        JSON["_STATISTICS_WRITING_APP"];
+  }
 
   if (!JSON["_STATISTICS_WRITING_DATE_UTC"].is_null()) {
     Log::debug({"[ProbeResultStreamTags.cpp] _STATISTICS_WRITING_DATE_UTC: ",
@@ -32,17 +44,21 @@ ProbeResultStreamTags::ProbeResultStreamTags(nlohmann::json JSON) {
         JSON["_STATISTICS_WRITING_DATE_UTC"];
   }
 
-  Log::debug({"[ProbeResultStreamTags.cpp] _STATISTICS_TAGS: ",
-              JSON["_STATISTICS_TAGS"]});
-  ProbeResultStreamTags::_STATISTICS_TAGS = JSON["_STATISTICS_TAGS"];
+  if (!JSON["_STATISTICS_TAGS"].is_null()) {
+    Log::debug({"[ProbeResultStreamTags.cpp] _STATISTICS_TAGS: ",
+                JSON["_STATISTICS_TAGS"]});
+    ProbeResultStreamTags::_STATISTICS_TAGS = JSON["_STATISTICS_TAGS"];
+  }
 
   if (!JSON["ENCODER"].is_null()) {
     Log::debug({"[ProbeResultStreamTags.cpp] ENCODER: ", JSON["ENCODER"]});
     ProbeResultStreamTags::ENCODER = JSON["ENCODER"];
   }
+
   if (!JSON["DURATION"].is_null()) {
     Log::debug({"[ProbeResultStreamTags.cpp] DURATION: ", JSON["DURATION"]});
     ProbeResultStreamTags::DURATION = JSON["DURATION"];
   }
+
   Log::debug({"[ProbeResultStreamTags.cpp] Ended ProbeResultStreamTags"});
 }
