fc179487bedd1b89b88e81b5a582404a1fcc7e7f
boooiil <boooiil@outlook.com>
2024-03-02 00:22:49 -0500

    base class for float-based arguments

---
+++ src/application/arguments/FloatArgument.h
@@ -0,0 +1,15 @@
+#ifndef FLOAT_ARGUMENT_H
+#define FLOAT_ARGUMENT_H
+
+#include "./BaseArgument.h"
+
+class FloatArgument : public BaseArgument<float> {
+ public:
+  operator std::string(void) const { return std::to_string(value); };
+  operator const char*(void) const { return std::to_string(value).c_str(); };
+
+  std::string asString(void) { return std::to_string(value); };
+  const char* c_str(void) { return asString().c_str(); };
+};
+
+#endif  // FLOAT_ARGUMENT_H
