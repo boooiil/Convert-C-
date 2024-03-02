6c3039933acf5abe9a3192591c457d03db4c9e57
boooiil <boooiil@outlook.com>
2024-03-02 00:22:19 -0500

    base class for bool-based arguments

---
+++ src/application/arguments/FlagArgument.h
@@ -0,0 +1,19 @@
+#ifndef FLAG_ARGUMENT_H
+#define FLAG_ARGUMENT_H
+
+#include "./BaseArgument.h"
+
+class FlagArgument : public BaseArgument<bool> {
+ public:
+  operator std::string(void) const { return value ? "true" : "false"; };
+  operator const char*(void) const { return value ? "true" : "false"; };
+
+  bool operator!(void) const { return !value; };
+
+  std::string toString() const { return value ? "true" : "false"; };
+  const char* c_str() const { return toString().c_str(); };
+
+  virtual FlagArgument& operator=(const bool& provided) = 0;
+};
+
+#endif  // FLAG_ARGUMENT_H
