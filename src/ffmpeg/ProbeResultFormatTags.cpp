43a88af7706ab9c1b3d70ade8c1bad89280dd378
boooiil <boooiil@outlook.com>
2024-04-17 11:54:47 -0400

    debugging

--- src/ffmpeg/ProbeResultFormatTags.cpp
+++ src/ffmpeg/ProbeResultFormatTags.cpp
@@ -1,8 +1,11 @@
 #include "ProbeResultFormatTags.h"
 
+#include "../logging/Log.h"
+
 ProbeResultFormatTags::ProbeResultFormatTags() {}
 ProbeResultFormatTags::~ProbeResultFormatTags() {}
 
 ProbeResultFormatTags::ProbeResultFormatTags(nlohmann::json JSON) {
+  Log::debug({"[ProbeResultFormatTags.cpp] starting format tags"});
   // ProbeResultFormatTags::ENCODER = JSON["ENCODER"];
 }
