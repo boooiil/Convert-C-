6d32e9733dbfed92e73fcc179af0443d10bd463b
boooiil <boooiil@outlook.com>
2024-04-17 11:51:11 -0400

    attempt to cleanly catch problems with parsing JSON

--- src/media/MediaProcessStatistics.cpp
+++ src/media/MediaProcessStatistics.cpp
@@ -61,59 +61,67 @@ void MediaProcessStatistics::start(std::string command) {
 }
 
 void MediaProcessStatistics::parse(std::string data) {
-  nlohmann::json JSON = nlohmann::json::parse(data);
-  this->media->probeResult = new ProbeResult(JSON);
+  try {
+    nlohmann::json JSON = nlohmann::json::parse(data);
+    this->media->probeResult = new ProbeResult(JSON);
 
-  assert(this->media->probeResult->videoStreams.size() > 0);
+    assert(this->media->probeResult->videoStreams.size() > 0);
 
-  ProbeResultStreamVideo prsv = this->media->probeResult->videoStreams[0];
+    ProbeResultStreamVideo prsv = this->media->probeResult->videoStreams[0];
 
-  std::istringstream rateStream(
-      this->media->probeResult->videoStreams[0].r_frame_rate);
-  int numerator, denominator;
-  char slash;
-  rateStream >> numerator >> slash >> denominator;
+    std::istringstream rateStream(
+        this->media->probeResult->videoStreams[0].r_frame_rate);
+    int numerator, denominator;
+    char slash;
+    rateStream >> numerator >> slash >> denominator;
 
-  int hours;
-  int minutes;
-  int seconds;
-  int duration;
+    int hours;
+    int minutes;
+    int seconds;
+    int duration;
 
-  if (!prsv.tags.DURATION.empty()) {
-    // TODO: calc duration??
-    std::vector<std::string> timeParts =
-        ListUtils::splitv(prsv.tags.DURATION, std::string(":"));
+    if (!prsv.tags.DURATION.empty()) {
+      // TODO: calc duration??
+      std::vector<std::string> timeParts =
+          ListUtils::splitv(prsv.tags.DURATION, std::string(":"));
 
-    hours = std::stoi(timeParts[0]);
-    minutes = std::stoi(timeParts[1]);
-    seconds = std::stoi(timeParts[2]);
-    duration = (hours * 60 * 60) + (minutes * 60) + seconds;
-  } else {
-    Log::debug({"[MediaProcessStatistics.cpp] DURATION NOT FOUND"});
-    Log::debug(
-        {"[MediaProcessStatistics.cpp] Obtaining duration from format. This "
-         "could be inaccurate."});
+      hours = std::stoi(timeParts[0]);
+      minutes = std::stoi(timeParts[1]);
+      seconds = std::stoi(timeParts[2]);
+      duration = (hours * 60 * 60) + (minutes * 60) + seconds;
+    } else {
+      Log::debug({"[MediaProcessStatistics.cpp] DURATION NOT FOUND"});
+      Log::debug(
+          {"[MediaProcessStatistics.cpp] Obtaining duration from format. This "
+           "could be inaccurate."});
 
-    duration = (int)std::stof(this->media->probeResult->format.duration);
+      duration = (int)std::stof(this->media->probeResult->format.duration);
+    }
+
+    this->media->file->size =
+        std::stoull(this->media->probeResult->format.size);
+    this->media->video->fps =
+        round((static_cast<float>(numerator) / denominator) * 100) / 100;
+    this->media->video->width = prsv.width;
+    this->media->video->height = prsv.height;
+    this->media->video->totalFrames =
+        static_cast<int>(std::ceil(duration * this->media->video->fps));
+
+    assert(!this->container->userSettings.quality.get().name.empty());
+
+    MediaFormat format = container->userSettings.quality;
+
+    this->media->video->convertedWidth = std::to_string(format.width);
+    this->media->video->convertedHeight = std::to_string(format.getResolution(
+        this->media->video->width, this->media->video->height, format.width));
+    this->media->video->convertedResolution =
+        this->media->video->convertedWidth + ":" +
+        this->media->video->convertedHeight;
+    this->media->video->crf = format.crf;
+  } catch (const std::exception& e) {
+    Log::debug({"[MediaProcessStatistics.cpp] ERROR: ", e.what()});
+    this->media->setActivity(Activity::FAILED);
+    MediaProcessStatistics::status = MediaProcess::Status::_ERROR;
+    return;
   }
-
-  this->media->file->size = std::stoull(this->media->probeResult->format.size);
-  this->media->video->fps =
-      round((static_cast<float>(numerator) / denominator) * 100) / 100;
-  this->media->video->width = prsv.width;
-  this->media->video->height = prsv.height;
-  this->media->video->totalFrames =
-      static_cast<int>(std::ceil(duration * this->media->video->fps));
-
-  assert(!this->container->userSettings.quality.get().name.empty());
-
-  MediaFormat format = container->userSettings.quality;
-
-  this->media->video->convertedWidth = std::to_string(format.width);
-  this->media->video->convertedHeight = std::to_string(format.getResolution(
-      this->media->video->width, this->media->video->height, format.width));
-  this->media->video->convertedResolution = this->media->video->convertedWidth +
-                                            ":" +
-                                            this->media->video->convertedHeight;
-  this->media->video->crf = format.crf;
 }
