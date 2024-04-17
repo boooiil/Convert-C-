067053aae4d56cbb1c8fd40c57b09a81a0c91b57
boooiil <boooiil@outlook.com>
2024-04-17 11:40:32 -0400

    init -- unfinished

---
+++ src/application/arguments/EnumArgument.h
@@ -0,0 +1,15 @@
+#ifndef ENUM_ARGUMENT_H
+#define ENUM_ARGUMENT_H
+
+#include <type_traits>
+
+#include "./BaseArgument.h"
+
+template <typename T,
+          typename std::enable_if<std::is_base_of<T, T>::value>::type* =
+              nullptr>
+class EnumArgument : BaseArgument<T> {
+ public:
+};
+
+#endif  // ENUM_ARGUMENT_H
