2b5436fc46a3abe7e330d1f57813a85dbea39ad8
boooiil <boooiil@outlook.com>
2024-07-02 02:54:11 -0400

    rm ChildConversionProcess

--- src/program/parent/application/child/ChildConversionProcess.cpp
+++
@@ -1,6 +0,0 @@
-#include "ChildConversionProcess.h"
-
-ChildConversionProcess::ChildConversionProcess() {}
-ChildConversionProcess::~ChildConversionProcess() {}
-
-void ChildConversionProcess::parse(nlohmann::json JSON) {}
