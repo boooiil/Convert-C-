c59792f0fed0544a30bf7b170d4023da99145704
boooiil <boooiil@outlook.com>
2024-07-02 02:58:52 -0400

    add prepare, run, and end function decl

--- src/program/child/Child.h
+++ src/program/child/Child.h
@@ -1,10 +1,23 @@
 #ifndef CHILD_H
 #define CHILD_H
 
+#include <queue>
+
+#include "./media/Media.h"
+
 class Child {
  public:
-  Child();
-  ~Child();
+  Child(void);
+  ~Child(void);
+
+  /// @brief Holds current media files being converted.
+  std::queue<Media*> converting;
+  /// @brief Holds media files that are waiting to be converted.
+  std::queue<Media*> pending;
+
+  void prepare(void);
+  void run(void);
+  void end(void);
 };
 
 #endif  // !CHILD_H
--- src/program/parent/Parent.h
+++ src/program/parent/Parent.h
@@ -1,10 +1,20 @@
 #ifndef PARENT_H
 #define PARENT_H
 
+#include <queue>
+
+#include "child/ChildProcess.h"
+
 class Parent {
  public:
-  Parent();
-  ~Parent();
-}
+  Parent(void);
+  ~Parent(void);
+
+  std::queue<ChildProcess*> childProcesses;
+
+  void prepare(void);
+  void run(void);
+  void end(void);
+};
 
 #endif  // !PARENT_H
