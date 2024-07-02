7c848e3def1aacc8b2e41f4a77d8678311fe6b7f
boooiil <boooiil@outlook.com>
2024-07-02 03:02:13 -0400

    implement programSettings init and deletion & debugging

--- src/program/settings/Settings.cpp
+++ src/program/settings/Settings.cpp
@@ -3,13 +3,26 @@
 #include "../../logging/Log.h"
 
 Settings::Settings(int argc, char* argv[]) {
+
+  Log::debug({"[Settings.cpp] Creating settings object with (",
+              std::to_string(argc), ") args."});
+
+  Log::debug({"[Settings.cpp] Creating argument parser object."});
   this->argumentParser = new ArgumentParser();
   this->argumentParser->parse(argc, argv);
+
+  Log::debug({"[Settings.cpp] Creating program settings object."});
+  this->programSettings = new ProgramSettings();
+  this->programSettings->applySettings(*this->argumentParser);
 }
 
 Settings::~Settings() {
   if (this->argumentParser != nullptr) {
     Log::debug({"[Settings.cpp] Deleting argument parser"});
     delete this->argumentParser;
+  }
+  if (this->programSettings != nullptr) {
+    Log::debug({"[Settings.cpp] Deleting program settings"});
+    delete this->programSettings;
   }
 }
