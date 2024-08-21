ee1c327cb3e2f89ef5b8990d4bae223623ffff1c
boooiil <boooiil@outlook.com>
2024-08-21 15:49:35 -0400

    extend jsonserializablerunner

--- src/program/child/Child.h
+++ src/program/child/Child.h
@@ -1,11 +1,13 @@
 #ifndef CHILD_H
 #define CHILD_H
 
+#include <nlohmann/json_fwd.hpp>
 #include <queue>
 
+#include "../generics/JSONSerializableRunner.h"
 #include "./media/Media.h"
 
-class Child {
+class Child : public JSONSerializableRunner {
  public:
   Child(void);
   ~Child(void);
@@ -15,9 +17,16 @@ class Child {
   /// @brief Holds media files that are waiting to be converted.
   std::queue<Media*> pending;
 
+  /// @brief Prepares the child for conversion.
   void prepare(void);
+  ///  @brief Runs the child's conversion process.
   void run(void);
+  /// @brief Ends the child's conversion process.
   void end(void);
+
+  nlohmann::json asJSON(void);
+
+ private:
 };
 
 #endif  // !CHILD_H
