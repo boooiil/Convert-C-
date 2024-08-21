904247ef5e542ce426e79df59690f879f5ec8b05
boooiil <boooiil@outlook.com>
2024-08-21 15:40:42 -0400

    add comments, extend jsonserializable, set converting and pending queues

--- src/program/parent/Parent.h
+++ src/program/parent/Parent.h
@@ -1,20 +1,37 @@
 #ifndef PARENT_H
 #define PARENT_H
 
+#include <nlohmann/json_fwd.hpp>
 #include <queue>
 
+#include "../generics/JSONSerializableRunner.h"
 #include "child/ChildProcess.h"
+#include "ticker/ParentTicker.h"
 
-class Parent {
+class Parent : public JSONSerializableRunner {
  public:
-  Parent(void);
   ~Parent(void);
 
-  std::queue<ChildProcess*> childProcesses;
+  /// @brief Current converting child processes.
+  std::queue<ChildProcess*> converting;
+  /// @brief Pending child processes.
+  std::queue<ChildProcess*> pending;
 
+  /// @brief Run preparation tasks for parent.
   void prepare(void);
+  ///  @brief Run parent process.
   void run(void);
+  ///  @brief End parent process.
   void end(void);
+
+  /**
+   * @brief Returns the JSON representation of this object.
+   *
+   * @return nlohmann::json
+   */
+  nlohmann::json asJSON(void);
+
+ private:
 };
 
 #endif  // !PARENT_H
