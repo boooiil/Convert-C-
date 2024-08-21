66e2cc3b46e2f2097d7f1eb72e2f0a9f1e56402a
boooiil <boooiil@outlook.com>
2024-08-21 15:38:46 -0400

    init display for parent output

---
+++ src/program/parent/display/ParentDisplay.cpp
@@ -0,0 +1,13 @@
+#include "ParentDisplay.h"
+
+void ParentDisplay::print(void) {
+  // TODO: finish
+}
+
+void ParentDisplay::printDebug(void) {
+  // TODO: finish
+}
+
+void ParentDisplay::printJSON(void) {
+  // TODO: finish
+}
---
+++ src/program/parent/display/ParentDisplay.h
@@ -0,0 +1,13 @@
+#ifndef PARENT_DISPLAY_H
+#define PARENT_DISPLAY_H
+
+#include "../../generics/GenericDisplay.h"
+
+class ParentDisplay : public GenericDisplay {
+ public:
+  void print(void);
+  void printDebug(void);
+  void printJSON(void);
+};
+
+#endif  // !PARENT_DISPLAY_H
