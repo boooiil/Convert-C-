867dac66a03660033e4a7e327691bf5c12c3a45f
boooiil <boooiil@outlook.com>
2024-08-21 15:35:35 -0400

    interface for display-like classes

---
+++ src/program/generics/GenericDisplay.h
@@ -0,0 +1,11 @@
+#ifndef GENERIC_DISPLAY_H
+#define GENERIC_DISPLAY_H
+
+class GenericDisplay {
+ public:
+  virtual ~GenericDisplay(void) = default;
+  virtual void print(void) = 0;
+  virtual void printDebug(void) = 0;
+  virtual void printJSON(void) = 0;
+};
+#endif  // !GENERIC_DISPLAY_H
