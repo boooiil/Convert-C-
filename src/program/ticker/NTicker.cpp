e134e7c95b0d322225d4e7dc69937f7c1c7eadc8
boooiil <boooiil@outlook.com>
2024-09-08 00:48:04 -0400

    make JSONSerializable, implement all fns

--- src/program/ticker/NTicker.cpp
+++ src/program/ticker/NTicker.cpp
@@ -1,11 +1,35 @@
 #include "NTicker.h"
 
+#include <chrono>
+#include <thread>
+
+#include "../../logging/Log.h"
 #include "../Program.h"
+#include "../child/Child.h"
 #include "../child/display/ChildDisplay.h"
+#include "../parent/Parent.h"
 #include "../parent/display/ParentDisplay.h"
 #include "../settings/Help.h"
+#include "nlohmann/json.hpp"
+
+NTicker::NTicker(void) : endable(true) {
+  this->display = nullptr;
+  this->runner = nullptr;
+}
 
 void NTicker::determineNextAction(void) {
+  if (Program::settings->argumentParser->printHelp) {
+    Help::printHelp();
+    Program::stopFlag = true;
+  } else if (Program::settings->argumentParser->printInformation) {
+    // print information
+    this->runner->prepare();
+    this->display->printInformation();
+    Program::stopFlag = true;
+  } else {
+    // prepare runner
+    this->runner->prepare();
+  }
   // if (print help) display help
   // if (print information) print information
   //   print media information for child
@@ -17,32 +41,83 @@ void NTicker::determineNextAction(void) {
 void NTicker::prepare(void) {
   // use parent display
   if (Program::settings->argumentParser->isParent) {
+    Log::debug({"[NTicker.cpp] Running as parent."});
     this->display = new ParentDisplay();
+    this->runner = new Parent();
   } else {
+    Log::debug({"[NTicker.cpp] Running as child."});
     this->display = new ChildDisplay();
+    this->runner = new Child();
   }
+
+  this->determineNextAction();
 }
 
-void NTicker::start(void) {
-  if (Program::settings->argumentParser->printHelp) {
-    Help::printHelp();
-  }
+void NTicker::run(void) {
+  while (!Program::stopFlag) {
+    this->runner->run();
+    this->display->print();
 
-  while (true) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(
+        Program::settings->argumentParser->displayRefresh));
   }
-  if (Program::settings->argumentParser->isParent) {
-    // parent display
-    this->display->print();
-  } else {
-    // child display
-    this->display->print();
-  }
+  // if (Program::settings->argumentParser->isParent) {
+  //   // parent display
+  //   this->display->print();
+  // } else {
+  //   // child display
+  //   this->display->print();
+  // }
 }
 
 void NTicker::end(void) {
+  Log::debug({"[NTicker.cpp] Ending ticker."});
+  Log::debug({"[NTicker.cpp] Expected to delete { display, runner }."});
+
   if (this->display != nullptr) {
+    Log::debug({"[NTicker.cpp] Deleting display."});
     delete this->display;
   }
+
+  if (this->runner != nullptr) {
+    while (!this->runner->isEndable()) {
+      Log::debug({"[NTicker.cpp] Waiting for runner to end."});
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+      // while (!Program::ticker->runner->isEndable()
+      // wait for runner to end
+    }
+    this->runner->end();
+
+    Log::debug({"[NTicker.cpp] Deleting runner."});
+    delete this->runner;
+  }
+}
+
+void NTicker::setEndable(bool flag) {
+  Log::debug(
+      {"Parent has been set as endable:", this->endable ? "True" : "False"});
+  this->endable = flag;
+}
+
+bool NTicker::isEndable(void) { return this->endable; }
+
+void NTicker::fromJSON(nlohmann::json) {
+  // read json information
+}
+
+nlohmann::json NTicker::asJSON(void) {
+  using namespace nlohmann;
+
+  json program;
+
+  if (this->runner == nullptr) {
+    program["Runner"] = {};
+  } else {
+    program["Runner"] = this->runner->asJSON();
+  }
+
+  return program;
 }
 
 void NTicker::writeDebug(void) {
--- src/program/ticker/NTicker.h
+++ src/program/ticker/NTicker.h
@@ -1,19 +1,31 @@
-#pragma once
+#ifndef NTICKER_H
+#define NTICKER_H
+
+#include <nlohmann/json_fwd.hpp>
 
 #include "../generics/GenericDisplay.h"
-#include "../generics/GenericRunner.h"
+#include "../generics/JSONSerializableRunner.h"
 
-class NTicker : GenericRunner {
+class NTicker : public JSONSerializableRunner {
  public:
+  JSONSerializableRunner* runner;
+
+  NTicker(void);
   void determineNextAction(void);
   void prepare(void);
-  void start(void);
+  void run(void);
   void end(void);
 
-  const char* getType(void) const;
-  void setType(const char*);
+  void setEndable(bool endable);
+  bool isEndable(void);
+
+  void fromJSON(nlohmann::json);
+  nlohmann::json asJSON(void);
 
  private:
+  bool endable;
   GenericDisplay* display;
   void writeDebug(void);
 };
+
+#endif  // !NTICKER_H
