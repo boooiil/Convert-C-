b51c4ed32cfedf5ff949d05d9e964b920433c916
boooiil <boooiil@outlook.com>
2024-09-08 00:50:05 -0400

    interface for runnable-like objects

--- src/program/generics/GenericRunner.h
+++ src/program/generics/GenericRunner.h
@@ -1,11 +1,23 @@
 #ifndef GENERIC_RUNNER_H
 #define GENERIC_RUNNER_H
 
+#include <type_traits>
+
 class GenericRunner {
  public:
   virtual void prepare(void) = 0;
   virtual void run(void) = 0;
   virtual void end(void) = 0;
+
+  virtual void setEndable(bool flag) = 0;
+  virtual bool isEndable(void) = 0;
+
+  template <typename T>
+  T* getRunner(void) {
+    static_assert(std::is_base_of<GenericRunner, T>::value,
+                  "T must be a subclass of GenericRunner");
+    return dynamic_cast<T*>(this);
+  };
 };
 
 #endif  // !GENERIC_RUNNER_H
