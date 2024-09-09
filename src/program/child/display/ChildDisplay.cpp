08e2b34ad64e1ffbe3fae704f0440cf78798e361
boooiil <boooiil@outlook.com>
2024-09-08 21:59:29 -0400

    implement default print

--- src/program/child/display/ChildDisplay.cpp
+++ src/program/child/display/ChildDisplay.cpp
@@ -1,12 +1,25 @@
 #include "ChildDisplay.h"
 
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
 #include <queue>
 #include <string>
 
 #include "../../../logging/Log.h"
 #include "../../../logging/LogColor.h"
+#include "../../../utils/NumberUtils.h"
+#include "../../../utils/StringUtils.h"
+#include "../../../utils/TimeUtils.h"
 #include "../../Program.h"
 #include "../../generics/GenericRunner.h"
+#include "../../settings/ProgramSettings.h"
+#include "../../settings/arguments/ArgumentParser.h"
+#include "../../settings/enums/Activity.h"
+#include "../../settings/enums/Encoders.h"
+#include "../../settings/enums/HWAccelerators.h"
+#include "../../settings/enums/LoggingOptions.h"
+#include "../../settings/enums/Tunes.h"
 #include "../../ticker/NTicker.h"
 #include "../Child.h"
 #include "../ffmpeg/ProbeResultStreamAudio.h"
@@ -15,7 +28,163 @@
 #include "../media/Media.h"
 
 void ChildDisplay::print(void) {
-  // TODO: finish
+  Child child =
+      *Program::ticker->getRunner<NTicker>()->runner->getRunner<Child>();
+  ArgumentParser argumentParser = *Program::settings->argumentParser;
+  ProgramSettings programSettings = *Program::settings->programSettings;
+  std::string sendStr = "";
+
+  // int bufferLen = static_cast<int>(child.converting.size()) +
+  //                 static_cast<int>(child.pending.size()) + 1;
+
+  std::string ob = LogColor::fgGray("[");
+  std::string cb = LogColor::fgGray("]");
+
+  std::string time = ob + LogColor::fgCyan("TIME") + cb + " " +
+                     TimeUtils::dateFormat(TimeUtils::getEpoch());
+  std::string encoder =
+      ob + LogColor::fgCyan("TARGET ENC") + cb + " " +
+      LogColor::fgGray(Encoders::getValue(argumentParser.wantedEncoder));
+  std::string runningEncoder =
+      ob + LogColor::fgCyan("ENC") + cb + " " +
+      LogColor::fgGray(Encoders::getValue(programSettings.runningEncoder));
+  std::string runningDecoder = ob + LogColor::fgCyan("ACC") + cb + " " +
+                               LogColor::fgGray(HWAccelerators::getValue(
+                                   programSettings.runningHWAccel));
+  std::string resolution = ob + LogColor::fgCyan("RES") + cb + " " +
+                           LogColor::fgGray(argumentParser.quality.get().name);
+  std::string tune = ob + LogColor::fgCyan("TUNE") + cb + " " +
+                     LogColor::fgGray(Tunes::getValue(argumentParser.tune));
+  std::string amount = ob + LogColor::fgCyan("AMOUNT") + cb + " " +
+                       LogColor::fgGray(std::to_string(argumentParser.amount));
+
+  std::string constrain = ob + LogColor::fgRed("CONSTRAIN") + cb;
+  std::string debug = ob + LogColor::fgRed("DEBUG") + cb;
+  std::string crop = ob + LogColor::fgRed("CROP") + cb;
+
+  std::string header = time + " " + encoder + " " + runningEncoder + " " +
+                       runningDecoder + " " + resolution + " " + tune + " " +
+                       amount;
+
+  if (argumentParser.useConstrain) {
+    header += " " + constrain;
+  }
+
+  if (argumentParser.loggingFormat == LoggingOptions::DEBUG) {
+    header += " " + debug;
+  }
+
+  if (argumentParser.crop) {
+    header += " " + crop;
+  }
+
+// clear console?
+#ifdef _WIN32
+  system("cls");
+#else
+  system("clear");
+#endif
+  sendStr += header + "\n";
+  // Program::log->sendBuffer(bufferLen, header);
+
+  std::queue<Media*> t_queue;
+
+  while (!child.converting.empty()) {
+    Media* media = child.converting.front();
+    child.converting.pop();
+
+    float mediaFPS = media->working->fps > 0 ? media->working->fps : 1;
+    int totalFrames = media->video->totalFrames;
+    long completedFrames = static_cast<long>(media->working->completedFrames);
+    int diff = static_cast<int>(
+        ceil((totalFrames - completedFrames) / mediaFPS) * 1000);
+
+    // create a time util to get this
+    std::string started = ob + LogColor::fgCyan("START") + cb + " " +
+                          TimeUtils::timeFormat(media->started);
+    // create a time util to get this
+    std::string eta = ob + LogColor::fgCyan("ETA") + cb + " " +
+                      TimeUtils::durationFormat(diff);
+
+    float crf = media->working->quality;
+    int v_crf = media->video->crf;
+
+    float workingFPS = media->working->fps;
+    float videoFPS = media->video->fps;
+
+    std::string fileName = ob + LogColor::fgCyan("FILE") + cb + " " +
+                           LogColor::fgGray(StringUtils::truncateString(
+                               media->file->conversionName, 25));
+
+    std::string activity = ob + LogColor::fgCyan("ACT") + cb + " " +
+                           Activity::getValue(media->getActivity());
+
+    std::string percent =
+        ob + LogColor::fgCyan("PROG") + cb + " " +
+        std::to_string(
+            (int)(((float)completedFrames / (float)totalFrames) * 100)) +
+        "%";
+
+    std::string cq = ob + LogColor::fgCyan("QUAL") + cb + " " +
+                     NumberUtils::formatNumber((v_crf / crf) * 100, 2) + "%";
+    std::string speed = ob + LogColor::fgCyan("SPEED") + cb + " " +
+                        NumberUtils::formatNumber(workingFPS / videoFPS, 2);
+
+    std::string bitrate =
+        ob + LogColor::fgCyan("BITRATE") + cb + " " +
+        NumberUtils::formatNumber(media->working->bitrate, 2) + "kb/s";
+
+    sendStr += fileName + " " + activity + " " + started + " " + percent + " " +
+               cq + " " + bitrate + " " + speed + " " + eta + "\n";
+    // Program::log->sendBuffer(
+    //     bufferLen, fileName + " " + activity + " " + started + " " + percent
+    //     + " " + cq + " " + bitrate + " " + speed + " " + eta);
+
+    t_queue.push(media);
+  }
+
+  child.converting = t_queue;
+  t_queue = std::queue<Media*>();
+
+  while (!child.pending.empty()) {
+    Media* media = child.pending.front();
+    child.pending.pop();
+
+    std::string fileName = ob + LogColor::fgCyan("FILE") + cb + " " +
+                           LogColor::fgGray(StringUtils::truncateString(
+                               media->file->conversionName, 25));
+
+    std::string activity = ob + LogColor::fgCyan("ACT") + cb + " " +
+                           Activity::getValue(media->getActivity());
+
+    if (media->hasFinished()) {
+      int calculatedSize = static_cast<int>(floor(
+          ((media->file->size - media->file->newSize) / media->file->size) *
+          100));
+
+      std::string ended = ob + LogColor::fgCyan("END") + cb + " " +
+                          TimeUtils::timeFormat(media->ended);
+      std::string elapsed =
+          ob + LogColor::fgCyan("ELAPSED") + cb + " " +
+          TimeUtils::durationFormat((media->ended - media->started) * 1000);
+
+      std::string reduced = ob + LogColor::fgCyan("REDUCED") + cb + " " +
+                            std::to_string(calculatedSize) + "%";
+
+      sendStr += fileName + " " + activity + " " + reduced + " " + ended + " " +
+                 elapsed + "\n";
+      // Program::log->sendBuffer(bufferLen, fileName + " " + activity + " " +
+      //                                         reduced + " " + ended + " " +
+      //                                         elapsed);
+    } else {
+      sendStr += fileName + " " + activity + "\n";
+      // Program::log->sendBuffer(bufferLen, fileName + " " + activity);
+    }
+
+    t_queue.push(media);
+  }
+  child.pending = t_queue;
+  Log::send({sendStr});
 }
 
 void ChildDisplay::printDebug(void) {
@@ -157,5 +326,12 @@ void ChildDisplay::printInformation(void) {
 }
 
 void ChildDisplay::printJSON(void) {
+  // clear console?
+#ifdef _WIN32
+  system("cls");
+#else
+  system("clear");
+#endif
   // TODO: finish
+  puts(Program::ticker->asJSON().dump().c_str());
 }
