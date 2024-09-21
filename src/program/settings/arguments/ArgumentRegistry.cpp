c5d4c6e55760fb573a70f5c3817c8d9e841fdbfa
boooiil <boooiil@outlook.com>
2024-09-21 08:46:59 -0400

    implement registry for arguments

---
+++ src/program/settings/arguments/ArgumentRegistry.cpp
@@ -0,0 +1,31 @@
+#include "./ArgumentRegistry.h"
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+#include "GenericArgument.h"
+
+std::unordered_map<std::string, std::shared_ptr<GenericArgument>>
+    ArgumentRegistry::arguments = {};
+
+void ArgumentRegistry::add(std::string flag,
+                           std::shared_ptr<GenericArgument> argument) {
+  arguments[flag] = argument;
+}
+
+void ArgumentRegistry::remove(std::string flag) { arguments.erase(flag); }
+
+void ArgumentRegistry::update(std::string flag,
+                              std::shared_ptr<GenericArgument> argument) {
+  arguments[flag] = argument;
+}
+
+std::shared_ptr<GenericArgument> ArgumentRegistry::get(std::string flag) {
+  return arguments[flag];
+}
+
+const std::unordered_map<std::string, std::shared_ptr<GenericArgument>>
+ArgumentRegistry::get_all() {
+  return arguments;
+}
---
+++ src/program/settings/arguments/ArgumentRegistry.h
@@ -0,0 +1,40 @@
+#ifndef ARGUMENT_REGISTRY_H
+#define ARGUMENT_REGISTRY_H
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+#include "./GenericArgument.h"
+
+class ArgumentRegistry {
+ public:
+  static void add(std::string flag, std::shared_ptr<GenericArgument> argument);
+  static void remove(std::string flag);
+  static void update(std::string flag,
+                     std::shared_ptr<GenericArgument> argument);
+  static std::shared_ptr<GenericArgument> get(std::string flag);
+  static const std::unordered_map<std::string, std::shared_ptr<GenericArgument>>
+  get_all();
+
+  template <typename T>
+  static std::shared_ptr<T> get_t(std::string flag) {
+    return std::dynamic_pointer_cast<T>(arguments[flag]);
+  }
+
+  using addFn = void (*)(std::string, std::shared_ptr<GenericArgument>);
+  using removeFn = void (*)(std::string);
+  using updateFn = void (*)(std::string, std::shared_ptr<GenericArgument>);
+  using getFn = std::shared_ptr<GenericArgument> (*)(std::string);
+  using getAllFn = const std::unordered_map<
+      std::string, std::shared_ptr<GenericArgument>> (*)();
+
+  template <typename T>
+  using getTFn = std::shared_ptr<T> (*)(std::string);
+
+ private:
+  static std::unordered_map<std::string, std::shared_ptr<GenericArgument>>
+      arguments;
+};
+
+#endif  // !ARGUMENT_REGISTRY_H
