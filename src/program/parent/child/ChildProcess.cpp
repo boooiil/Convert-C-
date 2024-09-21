337245be6d0fe9bb6546b166c88a9806a6e4e4bf
boooiil <boooiil@outlook.com>
2024-09-21 09:01:18 -0400

    implement fns

--- src/program/parent/child/ChildProcess.cpp
+++ src/program/parent/child/ChildProcess.cpp
@@ -4,18 +4,107 @@
 #include <nlohmann/json.hpp>
 #include <string>
 
+#include "../../../logging/Log.h"
+#include "../../child/media/Media.h"
+#include "../../settings/enums/Activity.h"
+#include "ChildProcessConversion.h"
+
 ChildProcess::ChildProcess(std::string path, std::string filename)
-    : path(path), filename(filename), pid(-1) {}
+    : path(path),
+      filename(filename),
+      pid(-1),
+      endable(true),
+      ended(0),
+      started(0),
+      activity(Activity::WAITING) {}
 
 ChildProcess::~ChildProcess(void) {}
 
-void ChildProcess::start(void) {}
+Activity::ActivityType ChildProcess::getActivity() { return this->activity; }
 
 void ChildProcess::getArgs(void) {
   std::cout << this->path << " arguments: ";
   std::getline(std::cin, this->args);
 
-  this->args = "-lf JSON " + this->args;
+  this->args = "-lf json " + this->args;
+}
+
+const bool ChildProcess::isProcessing() {
+  switch (this->activity) {
+    case Activity::STATISTICS:
+    case Activity::CONVERT:
+    case Activity::VALIDATE:
+      return true;
+    default:
+      return false;
+  }
+}
+
+const bool ChildProcess::hasFailed() {
+  switch (this->activity) {
+    case Activity::FAILED:
+    case Activity::FAILED_CODEC:
+    case Activity::FAILED_CONTAINER:
+    case Activity::FAILED_CORRUPT:
+    case Activity::FAILED_FILE:
+    case Activity::FAILED_FILE_MISSING:
+    case Activity::FAILED_FILE_NOT_RECOGNIZED:
+    case Activity::FAILED_FILE_PERMISSIONS:
+    case Activity::FAILED_HARDWARE:
+    case Activity::FAILED_INVALID_AUDIO_STREAMS:
+    case Activity::FAILED_JSON_PARSE:
+    case Activity::FAILED_SYSTEM:
+      return true;
+    default:
+      return false;
+  }
+}
+
+const bool ChildProcess::hasFinished() {
+  return this->activity == Activity::FINISHED;
+}
+
+const bool ChildProcess::isWaiting() {
+  return this->activity == Activity::WAITING;
+}
+
+const bool ChildProcess::isWaitingToConvert() {
+  return this->activity == Activity::WAITING_CONVERT;
+}
+
+void ChildProcess::prepare(void) {}
+
+void ChildProcess::run(void) {}
+
+void ChildProcess::doConversion(void) {
+  this->activity = Activity::CONVERT;
+
+  Log::debug({"[ChildProcess.cpp] Running conversion for:", this->path});
+
+  ChildProcessConversion conversion(this);
+  conversion.start(this->path + "/" + this->filename + " " + this->args);
+}
+
+void ChildProcess::end(void) {}
+
+void ChildProcess::setEndable(bool flag) { this->endable = flag; }
+
+bool ChildProcess::isEndable(void) { return this->endable; }
+
+void ChildProcess::fromJSON(nlohmann::json childProcess) {
+  nlohmann::json runner = childProcess["Runner"];
+
+  for (nlohmann::json mediaFile : runner["converting"]) {
+    Media* media = new Media();
+    media->fromJSON(mediaFile);
+    this->converting.push_back(media);
+  }
+
+  for (nlohmann::json mediaFile : runner["pending"]) {
+    Media* media = new Media();
+    media->fromJSON(mediaFile);
+    this->pending.push_back(media);
+  }
 }
 
 nlohmann::json ChildProcess::asJSON(void) {
--- src/program/parent/child/ChildProcess.h
+++ src/program/parent/child/ChildProcess.h
@@ -3,20 +3,77 @@
 
 #include <nlohmann/json_fwd.hpp>
 #include <string>
+#include <vector>
 
-class ChildProcess {
+#include "../../child/media/Media.h"
+#include "../../generics/JSONSerializableRunner.h"
+#include "../../settings/enums/Activity.h"
+
+class ChildProcess : public JSONSerializableRunner {
  public:
   ChildProcess(std::string path, std::string filename);
   ~ChildProcess(void);
 
   int pid;
+  long started;
+  long ended;
+
+  std::vector<Media*> converting;
+  std::vector<Media*> pending;
 
   std::string path;
   std::string filename;
   std::string args;
 
-  void start(void);
   void getArgs(void);
+
+  void doConversion(void);
+
+  /**
+   * @brief Get the activity type.
+   *
+   * @return Activity::ActivityType - Activity type.
+   */
+  Activity::ActivityType getActivity(void);
+
+  /**
+   * @brief Check if the current media is processing.
+   *
+   * @return true if the media activity is STATISTICS, CONVERT or VALIDATE.
+   * @return false if the media activity is WAITING-like, FINISHED, or
+   * FAILED-like.
+   */
+  const bool isProcessing(void);
+
+  /**
+   * @brief Check if the current media has failed processing.
+   *
+   * @return true if the media activity is FAILED-like.
+   */
+  const bool hasFailed(void);
+
+  /**
+   * @brief Check if the current media has failed processing.
+   *
+   * @return true if the media activity is FINISHED.
+   */
+  const bool hasFinished(void);
+
+  /**
+   * @brief Check if the current media is waiting to be processed.
+   *
+   * @return true if the media activity is WAITING.
+   */
+  const bool isWaiting(void);
+
+  /**
+   * @brief Check if the current media is waiting to convert.
+   *
+   * @return true if the media activity is WAITING_CONVERT.
+   */
+  const bool isWaitingToConvert(void);
+
+  void fromJSON(nlohmann::json childProcess);
 
   /**
    * @brief Returns the process as a JSON object.
@@ -24,6 +81,17 @@ class ChildProcess {
    * @return nlohmann::json
    */
   nlohmann::json asJSON(void);
+
+  // Inherited via GenericRunner
+  void prepare(void) override;
+  void run(void) override;
+  void end(void) override;
+  void setEndable(bool flag) override;
+  bool isEndable(void) override;
+
+ private:
+  Activity::ActivityType activity;
+  bool endable;
 };
 
 #endif  // !CHILD_PROCESS_H
