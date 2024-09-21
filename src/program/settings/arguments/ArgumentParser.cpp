eefb95dbaf34323030c1286e6c1e3d966d647683
boooiil <boooiil@outlook.com>
2024-09-21 08:51:25 -0400

    add args to registry, parse args using registry, map args as json

--- src/program/settings/arguments/ArgumentParser.cpp
+++ src/program/settings/arguments/ArgumentParser.cpp
@@ -1,23 +1,167 @@
 #include "ArgumentParser.h"
 
+#include <memory>
 #include <nlohmann/json.hpp>
+#include <nlohmann/json_fwd.hpp>
 #include <string>
+#include <vector>
 
 #include "../../../logging/Log.h"
 #include "../../../logging/LogColor.h"
 #include "../../../utils/StringUtils.h"
 #include "../../Program.h"
-#include "../enums/Decoders.h"
 #include "../enums/Encoders.h"
-#include "../enums/GPUProviders.h"
-#include "../enums/HWAccelerators.h"
 #include "../enums/LoggingOptions.h"
-#include "../enums/Platform.h"
 #include "../enums/Tunes.h"
+#include "./ArgumentRegistry.h"
+#include "FlagArgument.h"
+#include "GenericArgument.h"
+#include "IntegerArgument.h"
+#include "StringArgument.h"
+#include "TimeStringArgument.h"
+#include "TimeStringVectorArgument.h"
+#include "VectorArgument.h"
 
-ArgumentParser::ArgumentParser() : tune(Tunes::Tune::FILM), validate(true) {}
+ArgumentParser::ArgumentParser() : tune(Tunes::Tune::FILM) {}
 
 ArgumentParser::~ArgumentParser(void) {}
+
+void ArgumentParser::prepare() {
+  ArgumentRegistry::addFn add = ArgumentRegistry::add;
+
+  std::shared_ptr<IntegerArgument> ia = std::make_shared<IntegerArgument>(
+      "-a", "--amount", "Amount of media to process", 1);
+
+  add("-a", ia);
+  add("--amount", ia);
+
+  std::shared_ptr<VectorArgument<int>> iva = std::make_shared<
+      VectorArgument<int>>(
+      "-ac", "--audiochannels",
+      "Specify number of audio channels for each audio stream. (Will default "
+      "to original channel count if not specified)",
+      std::vector<int>());
+
+  add("-ac", iva);
+  add("--audiochannels", iva);
+
+  std::shared_ptr<VectorArgument<std::string>> sva =
+      std::make_shared<VectorArgument<std::string>>(
+          "-aco", "--audiocodec",
+          "Specify audio formats for each audio stream. (Will default to "
+          "original "
+          "format if not specified)",
+          std::vector<std::string>());
+
+  add("-aco", sva);
+  add("--audiocodec", sva);
+
+  iva = std::make_shared<VectorArgument<int>>(
+      "-as", "--audiostreams",
+      "Specify audio streams to include in the "
+      "output. (Will default to all "
+      "streams if not specified)",
+      std::vector<int>());
+
+  add("-as", iva);
+  add("--audiostreams", iva);
+
+  std::shared_ptr<FlagArgument> fa =
+      std::make_shared<FlagArgument>("-b", "--bitrate",
+                                     "Use a bitrate instead of a quality "
+                                     "setting. (Will default to quality "
+                                     "if not specified)",
+                                     false);
+
+  add("-b", fa);
+  add("--bitrate", fa);
+
+  fa = std::make_shared<FlagArgument>(
+      "-c", "--crop", "Crop the video to the specified ratio.", false);
+
+  add("-c", fa);
+  add("--crop", fa);
+
+  fa = std::make_shared<FlagArgument>(
+      "-co", "--constrain", "Constrain the bitrate to the specified value.",
+      false);
+
+  add("-co", fa);
+  add("--constrain", fa);
+
+  ia = std::make_shared<IntegerArgument>(
+      "-crf", "--crf", "Set the CRF value for the output.", -1);
+
+  add("-crf", ia);
+  add("--crf", ia);
+
+  ia = std::make_shared<IntegerArgument>(
+      "-dr", "--displayrefresh",
+      "Set the display refresh rate for the output. (Will default to the "
+      "original refresh rate if not specified)",
+      1000);
+
+  add("-dr", ia);
+  add("--displayrefresh", ia);
+
+  // TODO: encoder (Encoders)
+
+  fa = std::make_shared<FlagArgument>("-h", "--help", "Print the help message.",
+                                      false);
+
+  add("-h", fa);
+  add("--help", fa);
+
+  fa = std::make_shared<FlagArgument>(
+      "-hwd", "--hardwaredecode", "Use hardware decoding if available.", true);
+
+  add("-hwd", fa);
+  add("--hardwaredecode", fa);
+
+  fa = std::make_shared<FlagArgument>(
+      "-hwe", "--hardwareencode", "Use hardware encoding if available.", false);
+
+  add("-hwe", fa);
+  add("--hardwareencode", fa);
+
+  fa = std::make_shared<FlagArgument>(
+      "-i", "--info", "Print information about the input file.", false);
+
+  add("-i", fa);
+  add("--info", fa);
+
+  // TODO: logging format (LoggingOptions)
+
+  fa = std::make_shared<FlagArgument>(
+      "-o", "--overwrite", "Overwrite the output file if it exists.", false);
+
+  add("-o", fa);
+  add("--overwrite", fa);
+
+  fa = std::make_shared<FlagArgument>("-parent", "-parent",
+                                      "Run as a parent process.", false);
+
+  add("-parent", fa);
+
+  // TODO: quality (MediaFormat)
+
+  std::shared_ptr<TimeStringArgument> sa = std::make_shared<TimeStringArgument>(
+      "-ss", "--start", "Start the video at the specified time.",
+      std::string());
+
+  add("-ss", sa);
+  add("--start", sa);
+
+  // TODO: tune (Tunes)
+
+  std::shared_ptr<TimeStringVectorArgument> tsva =
+      std::make_shared<TimeStringVectorArgument>(
+          "-tr", "--trim", "Trim the video to the specified time.",
+          std::vector<std::string>());
+
+  add("-tr", tsva);
+  add("--trim", tsva);
+}
 
 void ArgumentParser::parse(int argc, char* argv[]) {
   if (argc < 2) {
@@ -26,119 +170,28 @@ void ArgumentParser::parse(int argc, char* argv[]) {
 
   // Skip the first argument, which is the program name
   for (int i = 1; i < argc; i++) {
-    Log::debug({"[UserArguments.cpp] Option:", argv[i]});
+    Log::send({"[UserArguments.cpp] Option:", argv[i]});
     std::string option = StringUtils::toLowerCase(argv[i]);
 
-    // yandere dev moment
-    if (option == "-a" || option == "--amount") {
-      if (i + 1 >= argc) {
-        return invalidArgument("No value provided for argument.");
-      }
+    /**
+     * ***********************************
+     *
+     * OLD CHECK FOR ENUM ARGUMENTS
+     *
+     * ***********************************
+     */
 
-      this->amount.parse(argv[++i]);
-
-      if (this->amount.errored) {
-        return invalidArgument("Invalid amount provided.");
-      }
-    }
-
-    else if (option == "-ac" || option == "--audiochannels") {
-      if (i + 1 >= argc) {
-        return invalidArgument("No value provided for argument.");
-      }
-
-      this->audioChannels.parse(argv[++i]);
-
-      if (this->audioChannels.errored) {
-        return invalidArgument("Invalid audio channels.");
-      }
-    }
-
-    else if (option == "-af" || option == "--audioformats") {
-      if (i + 1 >= argc) {
-        return invalidArgument("No value provided for argument.");
-      }
-
-      this->audioFormats.parse(argv[++i]);
-
-      if (this->audioFormats.errored) {
-        return invalidArgument("Invalid audio formats.");
-      }
-    }
-
-    else if (option == "-as" || option == "--audiostreams") {
-      if (i + 1 >= argc) {
-        return invalidArgument("No value provided for argument.");
-      }
-
-      this->audioStreams.parse(argv[++i]);
-
-      if (this->audioStreams.errored) {
-        return invalidArgument("Invalid audio stream indexes.");
-      }
-    }
-
-    else if (option == "-b" || option == "--bitrate") {
-      this->useBitrate = true;
-    }
-
-    else if (option == "-c" || option == "--crop") {
-      this->crop = true;
-    }
-
-    else if (option == "-co" || option == "--constrain") {
-      this->useConstrain = true;
-    }
-
-    else if (option == "-crf" || option == "--crf") {
-      if (i + 1 >= argc) {
-        return invalidArgument("No value provided for argument.");
-      }
-
-      this->crfOverride.parse(argv[++i]);
-
-      if (this->crfOverride.errored) {
-        return invalidArgument("Invalid crf value provided.");
-      }
-    }
-
-    else if (option == "-dr" || option == "--display-refresh") {
-      if (i + 1 >= argc) {
-        return invalidArgument("No value provided for argument.");
-      }
-      this->displayRefresh.parse(argv[++i]);
-      if (this->displayRefresh.errored) {
-        return invalidArgument("Invalid display refresh value provided.");
-      }
-    }
-
-    else if (option == "-e" || option == "--encoder") {
+    if (option == "-e" || option == "--encoder") {
       if (i + 1 >= argc) {
         return invalidArgument("No value provided for argument.");
       }
 
       this->wantedEncoder.parse(argv[++i]);
 
-      if (this->wantedEncoder.errored) {
+      if (this->wantedEncoder.isErrored()) {
         return invalidArgument("Invalid encoder provided.");
       }
-
-    }
-
-    else if (option == "-h" || option == "--help") {
-      this->printHelp = true;
-    }
-
-    else if (option == "-hwd" || option == "hardwaredecode") {
-      this->useHardwareDecode = true;
-    }
-
-    else if (option == "-hwe" || option == "hardwareencode") {
-      this->useHardwareEncode = true;
-    }
-
-    else if (option == "-i" || option == "--info") {
-      this->printInformation = true;
+      continue;
     }
 
     else if (option == "-lf" || option == "--loggingformat") {
@@ -158,18 +211,10 @@ void ArgumentParser::parse(int argc, char* argv[]) {
 
       this->loggingFormat.parse(argv[++i]);
 
-      if (this->loggingFormat.errored) {
+      if (this->loggingFormat.isErrored()) {
         return invalidArgument("Invalid logging format provided.");
       }
-
-    }
-
-    else if (option == "-o" || option == "--overwrite") {
-      this->overwrite = true;
-    }
-
-    else if (option == "-parent") {
-      this->isParent = true;
+      continue;
     }
 
     else if (option == "-q" || option == "quality") {
@@ -179,22 +224,10 @@ void ArgumentParser::parse(int argc, char* argv[]) {
 
       this->quality.parse(argv[++i]);
 
-      if (this->quality.errored) {
+      if (this->quality.isErrored()) {
         return invalidArgument("Invalid quality provided.");
       }
-
-    }
-
-    else if (option == "-s" || option == "--start") {
-      if (i + 1 >= argc) {
-        return invalidArgument("No value provided for argument.");
-      }
-
-      this->startBeginning.parse(argv[++i]);
-
-      if (this->startBeginning.errored) {
-        return invalidArgument("Invalid start time provided.");
-      }
+      continue;
     }
 
     else if (option == "-t" || option == "--tune") {
@@ -209,22 +242,40 @@ void ArgumentParser::parse(int argc, char* argv[]) {
       }
 
       this->tune = tune_check;
+      continue;
     }
 
-    else if (option == "-tr" || option == "--trim") {
-      if (i + 1 >= argc) {
-        return invalidArgument("No value provided for argument.");
-      }
+    /**
+     * ***********************************
+     *
+     * NEW CHECK FOR REGISTERED ARGUMENTS
+     *
+     * ***********************************
+     */
 
-      this->trim.parse(argv[++i]);
+    std::shared_ptr<GenericArgument> argument = ArgumentRegistry::get(option);
 
-      if (this->trim.errored) {
-        return invalidArgument("Invalid trim times provided.");
-      }
-    }
-
-    else {
+    if (argument == nullptr) {
       invalidArgument(argv[i]);
+      continue;
+    }
+
+    std::shared_ptr<FlagArgument> flagArgument =
+        std::dynamic_pointer_cast<FlagArgument>(argument);
+
+    if (flagArgument != nullptr) {
+      flagArgument->parse("true");
+      continue;
+    } else {
+      argument->parse(argv[++i]);
+
+      if (argument->isErrored()) {
+        invalidArgument(std::string(argv[i - 1]) +
+                        " was provided invalid parameter " +
+                        std::string(argv[i]));
+        Log::send({argument->getHelpMessage()});
+        continue;
+      }
     }
   }
 }
@@ -235,30 +286,23 @@ void ArgumentParser::invalidArgument(std::string message) {
 }
 
 nlohmann::json ArgumentParser::asJSON() {
+  // TODO: implement visitor pattern for this
+
   nlohmann::json argumentParser;
 
-  argumentParser["audio_streams"] = this->audioStreams.get();
-  argumentParser["display_refresh"] = this->displayRefresh.get();
+  for (auto& [flag, argument] : ArgumentRegistry::get_all()) {
+    Log::send({flag, argument->getLongFlag(),
+               flag == argument->getLongFlag() ? "True" : "False"});
+    if (flag != argument->getLongFlag())
+      argumentParser[flag] = argument->toString();
+  }
+
   argumentParser["wanted_encoder"] =
       Encoders::getValue(this->wantedEncoder.get());
   argumentParser["quality"] = this->quality.get().scale;
   argumentParser["tune"] = Tunes::getValue(this->tune);
-  argumentParser["start_at"] = this->startBeginning.get();
-  argumentParser["trim"] = this->trim.get();
-  argumentParser["amount"] = this->amount.get();
-  argumentParser["crf_override"] = this->crfOverride.get();
-  argumentParser["crop"] = this->crop;
-  argumentParser["use_bitrate"] = this->useBitrate;
-  argumentParser["use_constrain"] = this->useConstrain;
-  argumentParser["validate"] = this->validate;
-  argumentParser["use_hardware_decode"] = this->useHardwareDecode;
-  argumentParser["use_hardware_encode"] = this->useHardwareEncode;
-  argumentParser["overwrite"] = this->overwrite;
   argumentParser["logging_format"] =
       LoggingOptions::getValue(this->loggingFormat);
-  argumentParser["is_parent"] = this->isParent;
-  argumentParser["print_information"] = this->printInformation;
-  argumentParser["print_help"] = this->printHelp;
 
   return argumentParser;
 }
--- src/program/settings/arguments/ArgumentParser.h
+++ src/program/settings/arguments/ArgumentParser.h
@@ -3,90 +3,27 @@
 
 #include <nlohmann/json_fwd.hpp>
 #include <string>
-#include <vector>
 
-#include "../arguments/encoding/AudioStreamIndexes.h"
-#include "../arguments/encoding/ConstantRateFactor.h"
-#include "../arguments/encoding/ConstrainBitrate.h"
 #include "../arguments/encoding/DesiredEncoder.h"
-#include "../arguments/encoding/ParallelAmount.h"
-#include "../arguments/encoding/UseBitrate.h"
-#include "../arguments/encoding/UseHardwareDecode.h"
-#include "../arguments/encoding/UseHardwareEncode.h"
-#include "../arguments/misc/IsParent.h"
 #include "../arguments/misc/LoggingFormat.h"
-#include "../arguments/misc/OverwriteExisting.h"
-#include "../arguments/misc/PrintHelp.h"
-#include "../arguments/misc/PrintInformation.h"
-#include "../arguments/video/Crop.h"
 #include "../arguments/video/Quality.h"
-#include "../arguments/video/StartAt.h"
-#include "../arguments/video/TrimBetween.h"
-#include "../enums/Decoders.h"
-#include "../enums/Encoders.h"
-#include "../enums/GPUProviders.h"
-#include "../enums/HWAccelerators.h"
-#include "../enums/Platform.h"
 #include "../enums/Tunes.h"
-#include "encoding/AudioChannels.h"
-#include "encoding/AudioFormats.h"
-#include "misc/DisplayRefresh.h"
 
 class ArgumentParser {
  public:
   ArgumentParser(void);
   ~ArgumentParser(void);
 
-  /// @brief The number of audio streams wanted.
-  AudioStreamIndexes audioStreams;
-  /// @brief The audio formats wanted.
-  AudioFormats audioFormats;
-  /// @brief The audio channels wanted.
-  AudioChannels audioChannels;
   /// @brief The current encoder being used
   Quality quality;
   /// @brief Codec tune setting
   Tunes::Tune tune;
-  /// @brief Start the video from this time
-  StartAt startBeginning;
-  /// @brief Trim the video to this time
-  TrimBetween trim;
   /// @brief The user's desired encoder
   DesiredEncoder wantedEncoder;
 
-  /// @brief Amount of concurrent conversions
-  ParallelAmount amount;
-  /// @brief CRF override
-  ConstantRateFactor crfOverride;
-
-  /// @brief Crop the video
-  Crop crop;
-  /// @brief Use bitrate instead of CRF
-  UseBitrate useBitrate;
-  /// @brief Use strict bitrate values instead of variable
-  ConstrainBitrate useConstrain;
-  /// @brief Validate the video after conversion
-  bool validate;
-  /// @brief Use hardware decode
-  UseHardwareDecode useHardwareDecode;
-  /// @brief Use hardware encode
-  UseHardwareEncode useHardwareEncode;
-  /// @brief Overwrite existing file
-  OverwriteExisting overwrite;
-  /// @brief Format for display output.
   LoggingFormat loggingFormat;
-  /// @brief Run as child process.
-  IsParent isParent;
 
-  /// @brief How often to refresh the display
-  DisplayRefresh displayRefresh;
-
-  /// @brief Print information about the video
-  PrintInformation printInformation;
-
-  /// @brief Print help information
-  PrintHelp printHelp;
-
+  void prepare(void);
   void parse(int argc, char* argv[]);
   void invalidArgument(std::string);
 
